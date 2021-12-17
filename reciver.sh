b97f9a81921a21de291608c962cec9d5f825b4fd
0Karakurt0 <karakurtspider.s0net@gmail.com>
2021-12-17 23:57:52 +0200

    Working update

--- reciver.sh
+++ reciver.sh
@@ -5,19 +5,32 @@ CHAT="$2"
 
 while true; do
     while read -r update; do
-        eval "$update"
+        echo -n "Parsing update..."
+        eval "$update" && \
+            echo "Done"
         if   [ $new_id != null ]; then
+            echo -n "Found new user, adding to list..."
             # Adding new user
-            echo "$new_id $date" >> ./list
+            if grep -q "$new_id" ./list; then
+                echo "Skipped."
+            else
+                echo "$new_id $date" >> ./list && \
+                    echo "Done"
+            fi
         elif [ $button != null ]; then
+            echo -n "Button was pressed, "
             # check if it's a correct button
-            if grep "$from" ./list; then
-                sed -i "/$from/d"
+            if grep -q "$from" ./list; then
+                echo -n " correct user. Removing from the list..."
+                sed -i "/$from/d" && \
+                echo "Done"
+            else
+                echo "wrong user"
             fi
         else
             sleep 5
         fi
     done <\
-         < curl -s -X GET "https://api.telegram.org/bot$token/getUpdates" \
-            | jq -r '.result[] | "date=\(.message.date);new_id=\(.message.new_chat_participant.id);button=\(.message.callback_query.data);from=\(.message.callback_query.from.id)"'
-
+         <(curl -s -X GET "https://api.telegram.org/bot$TOKEN/getUpdates" \
+         | jq -r '.result[] | "date=\(.message.date);new_id=\(.message.new_chat_participant.id);button=\(.message.callback_query.data);from=\(.message.callback_query.from.id);check=\(.message.callback_query.message.reply_to_message.from)"')
+ done
