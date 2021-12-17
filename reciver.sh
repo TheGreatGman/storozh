3e5e232e538ee9fab838140b06fa8b69f2057b49
0Karakurt0 <karakurtspider.s0net@gmail.com>
2021-12-17 23:34:04 +0200

    Reciver v1

--- reciver.sh
+++ reciver.sh
@@ -2,6 +2,22 @@
 
 TOKEN="$1"
 CHAT="$2"
+
 while true; do
-    answer=$( curl -s -X GET "https://api.telegram.org/bot$TOKEN/getUpdates" | jq
+    while read -r update; do
+        eval "$update"
+        if   [ $new_id != null ]; then
+            # Adding new user
+            echo "$new_id $date" >> ./list
+        elif [ $button != null ]; then
+            # check if it's a correct button
+            if grep "$from" ./list; then
+                sed -i "/$from/d"
+            fi
+        else
+            sleep 5
+        fi
+    done <\
+         < curl -s -X GET "https://api.telegram.org/bot$token/getUpdates" \
+            | jq -r '.result[] | "date=\(.message.date);new_id=\(.message.new_chat_participant.id);button=\(.message.callback_query.data);from=\(.message.callback_query.from.id)"'
 
