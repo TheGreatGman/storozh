537597b719ffaca4e220b9a37842b33c6d5931a7
0Karakurt0 <karakurtspider.s0net@gmail.com>
2021-12-18 00:48:13 +0200

    fix^2

--- banner.sh
+++ banner.sh
@@ -5,6 +5,6 @@ while read -r word1 word2 ; do
 difference=$(($(date +%s) - $word2))
     if (($difference > 60))
      then
-     $curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id=$chat -d user_id=$word1 -d revoke_messages=True
+     curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id=$chat -d user_id=$word1 -d revoke_messages=True
      fi
 done < ./list
